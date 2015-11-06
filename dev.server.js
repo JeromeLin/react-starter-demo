555f4a87b0e10aebb7be67394ffbdefb5b2a6f9c
JeromeLin <ji.lin@cloudinward.com>
2015-11-06 13:08:55 +0800

    dev-server port

--- dev.server.js
+++ dev.server.js
@@ -8,7 +8,7 @@ new WebpackDevServer(webpack(config), {
   hot: true,
   noInfo: false,
   historyApiFallback: true
-}).listen(3000, '127.0.0.1', function (err, result) {
+}).listen(3000, function (err, result) {
   if (err) {
     console.log(err);
   }
